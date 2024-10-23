de24c69c106166bcb4d3825dbb21e7f28d2c9bf6
JeyTix <tixi4615@gmail.com>
2024-10-23 09:43:35 -0500

    Final

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -1,86 +1,84 @@
 package controllers;
-
 import models.Persona;
-import views.View;;
-
+import views.View;
 public class Controller {
     private View view;
     private SortingMethods sortingMethods;
     private SearchMethods searchMethods;
     private Persona[] personas;
-
-    public Controller(View view, SortingMethods sortingMethods, SearchMethods searchMethods){
-        this.view= view;
-        this.sortingMethods= sortingMethods;
-        this.searchMethods= searchMethods;
-        System.out.println("Controller created");
+    public Controller(View view, SortingMethods sortingMethods, SearchMethods searchMethods) {
+        this.view = view;
+        this.searchMethods = searchMethods;
+        this.sortingMethods = sortingMethods;
+        this.personas = new Persona[0];
+        System.out.println("Controller Created");
     }
-
-    public void start(){
-        int option= 0;
+    public void start() {
+        int option;
         do {
-            option= view.showMenu();
+            option = view.showMenu();
             switch (option) {
                 case 1:
                     inputPersons();
                     break;
-
                 case 2:
-                    addPersons();
+                    //view.displayPersons(personas);
+                    // view.showPersons(personas);
+                    view.displayPersons(personas);
                     break;
-
                 case 3:
                     sortPersons();
                     break;
-
+                case 4:
+                    //searchPersonByAge();
+                    break;
+                case 5:
+                    //searchPersonByName();
+                    break;
                 case 100:
-                    System.out.println("Adios...");
+                    System.out.println("Adiós");
                     break;
-            
                 default:
-                System.out.println("Opcion no valida");
+                    System.out.println("Opción no válida");
                     break;
             }
-        } while (option!= 0);
+        } while (option != 0); 
     }
-    
-    public void inputPersons(){
-        int numeroPersonas= view.inputInt("Ingrese el numero de personas: ");
-        personas= new Persona[numeroPersonas];
-        for (int i = 0; i < numeroPersonas; i++) {
-            personas[i]= view.inputPersons();
-        }
-    }
-
-    public void addPersons(){
-        if(personas== null){
-            inputPersons();
-        } else{
-            int numeroPersonas= view.inputInt("Ingrese el numero de personar a adicionar: ");
-            Persona[] personasTotales= new Persona[personas.length+  numeroPersonas];
-            for (int i = 0; i < personasTotales.length; i++) {
-                personasTotales[i]= personas[i];
-            }
-            for (int i = personas.length; i < personasTotales.length; i++) {
-                personas[i]= view.inputPersons();
-            }
-
-            personas= personasTotales;
-        }
-    }
-
-    public void sortPersons(){
-        int sortingOption= view.selectSortingMethods();
-        if (sortingOption== 1) {
-            sortingMethods.sortByNameBubble(personas);
-        } else if (sortingOption== 2) {
-            sortingMethods.sortByAgeSelection(personas);
+    public void sortPersons() {
+        int sortingOption = view.selectSortingMethods();
+        if (sortingOption == 1) {
+            sortingMethods.sortByNameWithBubble(personas);
+        } else if (sortingOption == 2) {
+            sortingMethods.sortByNameWithSelection(personas);
+        } else if (sortingOption == 3) {
+            sortingMethods.sortByAgeWithBubble(personas); 
+        } else if (sortingOption == 4) {
+            sortingMethods.sortByAgeWithSelection(personas);
         } else {
-            view.showMessage("Opcion validad");        
+            view.showMessage("Opción no válida");
+        }
+        view.displayPersons(personas);
+    }
+    public void inputPersons() {
+        int numeroPersonas = view.inputInt("Ingrese el número de personas: ");
+        personas = new Persona[numeroPersonas]; 
+        for (int i = 0; i < numeroPersonas; i++) {
+            personas[i] = view.inputPersons(); 
         }
     }
-    
-    public void searchPersons(){
-
+    public void addPerson() {
+        if(personas == null) {
+            view.showMessage("No existen personas");
+            inputPersons();
+        }
+        int numeroPersonas = view.inputInt("Ingrese el número de personas a agregar: ");
+        Persona[] personasTotales = new Persona[personas.length + numeroPersonas];
+        for (int i = 0; i < personas.length; i++) {
+            personasTotales[i] = personas[i];
+        } 
+        for (int i = personas.length; i < personasTotales.length; i++) {
+            personasTotales[i] = view.inputPersons();
+        }
+        personas = personasTotales;
     }
 }
--- src/controllers/SortingMethods.java
+++ src/controllers/SortingMethods.java
@@ -4,31 +4,66 @@ import models.Persona;
 
 public class SortingMethods {
 
-    public void sortByNameBubble(Persona[] personas) {
-        int n = personas.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (personas[j].getName().compareTo(personas[j + 1].getName()) > 0) {
-                    Persona temp = personas[j];
-                    personas[j] = personas[j + 1];
-                    personas[j + 1] = temp;
+    // Método de burbuja para ordenar por nombre
+    public Persona[] sortByNameWithBubble(Persona[] personas) {
+        int tamanio = personas.length;
+        for (int i = 0; i < tamanio - 1; i++) {
+            for (int j = i + 1; j < tamanio; j++) {
+                if (personas[i].getName().compareTo(personas[j].getName()) > 0) {
+                    Persona aux = personas[i];
+                    personas[i] = personas[j];
+                    personas[j] = aux;
                 }
             }
         }
+        return personas;
+    }
+    // Método de selección para ordenar por nombre
+    public Persona[] sortByNameWithSelection(Persona[] personas) {
+        int tamanio = personas.length;
+
+        for (int i = 0; i < tamanio - 1; i++) {
+            int indice = i;
+            for (int j = i + 1; j < tamanio; j++) {
+                if (personas[j].getName().compareTo(personas[indice].getName()) < 0) {
+                    indice = j;
+                }
+            }
+            Persona aux = personas[indice];
+            personas[indice] = personas[i];
+            personas[i] = aux;
+        }
+        return personas;
     }
 
-    public void sortByAgeSelection(Persona[] personas) {
-        int n = personas.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIndex = i;
-            for (int j = i + 1; j < n; j++) {
-                if (personas[j].getAge() < personas[minIndex].getAge()) {
-                    minIndex = j;
+    // Método de burbuja para ordenar por edad
+    public Persona[] sortByAgeWithBubble(Persona[] personas) {
+        int tamanio = personas.length;
+        for (int i = 0; i < tamanio - 1; i++) {
+            for (int j = 0; j < tamanio - 1 - i; j++) {
+                if (personas[j].getAge() > personas[j + 1].getAge()) {
+                    Persona aux = personas[j];
+                    personas[j] = personas[j + 1];
+                    personas[j + 1] = aux;
                 }
             }
-            Persona temp = personas[minIndex];
-            personas[minIndex] = personas[i];
-            personas[i] = temp;
         }
+        return personas;
+    }
+    // Método de selección para ordenar por edad
+    public Persona[] sortByAgeWithSelection(Persona[] personas) {
+        int tamanio = personas.length;
+        for (int i = 0; i < tamanio - 1; i++) {
+            int indice = i;
+            for (int j = i + 1; j < tamanio; j++) {
+                if (personas[j].getAge() < personas[indice].getAge()) {
+                    indice = j;
+                }
+            }
+            Persona aux = personas[indice];
+            personas[indice] = personas[i];
+            personas[i] = aux;
+        }
+        return personas;
     }
 }
